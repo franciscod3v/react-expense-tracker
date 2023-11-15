0149cf8e90f4382058b9f9eeed2fc66a7e915332
franciscod3v <franciscod3v@gmail.com>
2023-11-15 18:01:32 -0500

    Se crear suma de ingresos

--- src/components/IncomeExpenses.jsx
+++ src/components/IncomeExpenses.jsx
@@ -1,7 +1,28 @@
+import { useGlobalState } from "../context/GlobalState"
 
 function IncomeExpenses() {
+
+    const { transactions } = useGlobalState()
+
+    const amounts = transactions.map(transaction => transaction.amount)
+
+    const income = amounts
+                    .filter(amount => amount > 0)
+                    .reduce((acc, item) => (acc += item), 0)
+    
+    console.log('Ingresos', income)
+
   return (
-    <div>IncomeExpenses</div>
+    <>
+        <div>
+            <h4>Income</h4>
+            <p>{2000}</p>
+        </div>
+        <div>
+            <h4>Expense</h4>
+            <p>{1500}</p>
+        </div>
+    </>
   )
 }
 
